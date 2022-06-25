5d86e2f4c91d305425c2b4fbcdb8b4b1397f2775
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-06-25 15:10:49 +0700

    [dialog] done

---
+++ app/src/main/java/com/example/task2_mobile/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.example.task2_mobile;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeActivity#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeActivity extends Fragment {
+    TextView textViewHome;
+    EditText editTextHome;
+    Button btnSubmitFragmentHome;
+    AlertDialog dialogFirstPage;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View HomeViewFragment = inflater.inflate(R.layout.fragment_home_activity, container, false);
+        textViewHome = HomeViewFragment.findViewById(R.id.tv_textFragment);
+        editTextHome = HomeViewFragment.findViewById(R.id.et_textFragment);
+        btnSubmitFragmentHome = HomeViewFragment.findViewById(R.id.btn_submitFragment);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Confirm").setMessage("Are you sure want to edit the title text?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                textViewHome.setText(editTextHome.getText().toString());
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogFirstPage.dismiss();
+                Toast.makeText(getActivity(), "Changes canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogFirstPage = builder.create();
+
+        btnSubmitFragmentHome.setOnClickListener(view -> {
+            dialogFirstPage.show();
+        });
+        return HomeViewFragment;
+        //Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_home_activity, container, false);
+    }
+}
--- app/src/main/java/com/example/task2_mobile/MainActivity.java
+++ app/src/main/java/com/example/task2_mobile/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.task2_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     Button Logout;
     TextView Username;
+    TabLayout tabLayout;
+    ViewPager2 viewPagerMain;
+    PageAdapter pageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         Logout = findViewById(R.id.btn_Logout);
         Username = findViewById(R.id.tv_Username);
+        tabLayout = findViewById(R.id.Tab_LayoutMain);
+        viewPagerMain = findViewById(R.id.viewPagerMain);
 
         Intent intent = getIntent();
         String tempUserName = intent.getStringExtra("account_userName");
@@ -29,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
             Intent LogoutIntent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(LogoutIntent);
         });
+
+        setViewPagerMain(viewPagerMain);
+        new TabLayoutMediator(tabLayout, viewPagerMain, ((tab, position) -> {
+            tab.setText(pageAdapter.getFragmentTitle(position));
+        })).attach();
+    }
+
+    private void setViewPagerMain(ViewPager2 viewPagerMain){
+        if(pageAdapter == null){
+            pageAdapter  = new PageAdapter(this);
+            pageAdapter.addFragment(new HomeActivity(), "Home");
+            pageAdapter.addFragment(new SecondpageMainFragment(), "Second");
+            viewPagerMain.setAdapter(pageAdapter);
+        }
     }
 
 }
